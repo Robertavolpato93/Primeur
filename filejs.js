a353f49bb5862d17d2057d7cb9e71a0e18f1e2ae
Roberta <robi.volpato93@gmail.com>
2023-03-19 09:38:46 +0100

    ultimi dettagli finiti

--- filejs.js
+++ filejs.js
@@ -1,3 +1,4 @@
+// prima funzione per chiamata xmlhttprequest tramite get
 function getAllTitles() {
     const xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://jsonplaceholder.typicode.com/todos');
@@ -5,6 +6,8 @@ function getAllTitles() {
       if (xhr.status === 200) {
         const response = JSON.parse(xhr.responseText);
         autocomplete(document.getElementById('input'), response);
+        
+        //salvataggio della response allínterno di un array
         const myarray=[]
         myarray.push(response)
         console.log(myarray);
@@ -13,48 +16,40 @@ function getAllTitles() {
     xhr.send();
   }
   
+  //Seconda funzione per implementare autocomplete
   function autocomplete(input, myarray) {
-  
     input.addEventListener('input', function () {
       
       closeList();
-  
-      if (!this.value)
+   if (!this.value)
           return;
   
-      
+      //Creazione div suggerimenti
       suggestions = document.createElement('div');
       suggestions.setAttribute('id', 'suggestions');
       this.parentNode.appendChild(suggestions);
    
-  
-      
-      for (let i=0; i<myarray.length; i++) {
-          
-            if (myarray[i].title.slice(0, 6).includes(this.value.slice(0, 6))) {
-            
-          
-  
-             
-              suggestion = document.createElement('div');
+   //ciclo contenuto array
+      for (let i=0; i < myarray.length; i++) {
+         
+        //Utilizzo del metodo slice per filtrare le lettere di interesse e matcharle
+     if (myarray[i].title.slice(0, 6).includes(this.value.slice(0, 6))) {
+           
+        // Accesso alla proprieta' title della response e salvataggio dei suggerimenti all'interno di ulteriori div
+          suggestion = document.createElement('div');
               suggestion.innerHTML = myarray[i].title;
-              
-              suggestion.addEventListener('click', function () {
-                  input.target.value = this.innerHTML;
-                  closeList();
-              });
               suggestion.style.cursor = 'pointer';
-  
-              suggestions.appendChild(suggestion);
+               suggestions.appendChild(suggestion);
              }
            }
+        });
   
-          });
-  
+        //funzione per chiudere div contenente lista
     function closeList() {
       let suggestions = document.getElementById('suggestions');
       if (suggestions)
           suggestions.parentNode.removeChild(suggestions);
-  }
-  }
+         }
+        }
+    //richiamo prima funzione
   getAllTitles();
